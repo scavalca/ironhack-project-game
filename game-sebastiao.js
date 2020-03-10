4ef80e435e586cf61cdb47780105c57f072ff9b8
sebagalindo <cmsgalindo@icloud.com>
2020-03-10 18:58:56 -0300

    updatting

--- game-sebastiao.js
+++ game-sebastiao.js
@@ -9,14 +9,18 @@ window.onload = () => {
   }
 }
 
-let imageBackground = new Image();
-imageBackground.src = './images/deepsea.jpg';
+// let imageBackground = new Image();
+// imageBackground.src = './images/deepsea.jpg';
 
 let imageCharacter = new Image();
 imageCharacter.src = './images/fireball.png';
 
 let imageObstacle = new Image();
 imageObstacle.src = './images/star.png';
+
+let soundGameOver = new Audio();
+soundGameOver.src = './images/gameoversound.mp3';
+
 
 let myObstacles = [];
 
@@ -25,6 +29,31 @@ let requestID = null;
 let myGameArea = {
   canvas: document.createElement("canvas"),
   frames: 0,
+
+  points: 0,
+
+  x: 0,
+  y: 0,
+  width: 1250,
+  height: 638,
+  speed: -2,
+
+  imageBackground: new Image(),
+
+  move: function () {
+    this.x += this.speed;
+    this.x %= this.width;
+  },
+
+  drawBackground: function () {
+    this.context = this.canvas.getContext("2d");
+    this.imageBackground.src = './images/deepsea.jpg';
+    this.context.drawImage(this.imageBackground, this.x, this.y, this.width, this.height);
+
+    if (this.speed < 0) {
+      this.context.drawImage(this.imageBackground, this.x + this.width, this.y, this.width, this.height);
+    }
+  },
 
   start: function () {
     //canvas size
@@ -43,7 +72,7 @@ let myGameArea = {
   // it will clear the game area before drawing again
   clear: function () {
     // this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.drawImage(imageBackground, 0, 0, this.canvas.width, this.canvas.height);
+    this.context.drawImage(this.imageBackground, 0, 0, this.canvas.width, this.canvas.height);
   },
 
   //   clearInterval(this.interval);
@@ -59,11 +88,12 @@ let myGameArea = {
   },
 
   score: function () {
-    let points = Math.floor(this.frames / 10);
+    // let points = Math.floor(this.frames / 10);
+    this.points = Math.floor(this.frames / 10);
     this.context.font = "30px serif";
-    this.context.fillStyle = "green";
-    this.context.fillText( 'Score ' + points, 50, 50);
-    // this.context.drawImage(imageStar, 0, 0);
+    this.context.fillStyle = "black";
+    this.context.fillText(this.points, 100, 50);
+    this.context.drawImage(imageCharacter, 30, 13, 50, 50);
   }
 }
 
@@ -85,22 +115,22 @@ class Character {
 
 
   newPos() {
-    if (this.x >= 0 && this.x <= 1185) {
+    if (this.x >= 0 && this.x <= 1170) {
       this.x += this.speedX;
     } else if (this.x < 0) {
       this.x = 0;
       this.speedX = 0;
-    } else if (this.x > 1185) {
-      this.x = 1185;
+    } else if (this.x > 1170) {
+      this.x = 1170;
       this.speedX = 0;
     }
-    if (this.y >= 0 && this.y <= 580) {
+    if (this.y >= 0 && this.y <= 560) {
       this.y += this.speedY;
     } else if (this.y < 0) {
       this.y = 0;
       this.speedY = 0;
-    } else if (this.y > 580) {
-      this.y = 580;
+    } else if (this.y > 560) {
+      this.y = 560;
       this.speedY = 0;
     }
   }
@@ -110,12 +140,6 @@ class Character {
     ctx.fillStyle = this.color;
     // ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(image, this.x, this.y, 80, 80);
-
-    // // let canvas = document.getElementById('canvas');
-    // // let context = canvas.getContext('2d');
-
-    // // image.onload = function () {
-    // // }
   }
 
   left() {
@@ -154,8 +178,11 @@ let character = new Character(60, 60, 'red', 70, 315);
 
 function updateGameArea() {
   myGameArea.clear();
+  myGameArea.drawBackground();
+  myGameArea.move();
   character.newPos();
   character.update(imageCharacter);
+  faster();
 
   // update the obstacles array
   updateObstacles();
@@ -186,14 +213,6 @@ document.onkeydown = function (e) {
       character.speedX += 1;
       break;
   }
-
-  // if (e.keyCode == 39) {
-  //   // console.log((character.x + 60), myGameArea.canvas.width);
-  //   if (character.x < myGameArea.canvas.width) {
-
-  //     character.speedX += 1;
-  //   } 
-  // }
 };
 
 // stop adding speed to the character
@@ -204,13 +223,30 @@ document.onkeyup = function (e) {
 
 // update obstacles
 
+let fast = 120;
+// console.log(points);
+
+function faster () {
+  if (myGameArea.points >= 50 && myGameArea.points < 99) {
+    fast = 100;
+  } else if (myGameArea.points >= 100 && myGameArea.points < 149) {
+    fast = 80;
+  } else if (myGameArea.points >= 149 && myGameArea.points < 199) {
+    fast = 60;
+  } else if (myGameArea.points >= 200 && myGameArea.points < 249) {
+    fast = 40;
+  } else if (myGameArea.points > 249) {
+    fast = 20;
+  }
+}
+
 function updateObstacles() {
   for (i = 0; i < myObstacles.length; i++) {
     myObstacles[i].y += 1;
     myObstacles[i].update(imageObstacle);
   }
   myGameArea.frames += 1;
-  if (myGameArea.frames % 180 === 0) {
+  if (myGameArea.frames % fast === 0) {
     let minWidth = 60;
     let maxWidth = 1190;
     let width = Math.floor(
@@ -225,13 +261,11 @@ function checkGameOver() {
   });
   if (crashed) {
     myGameArea.stop();
+    soundGameOver.play();
+    // function reload
+    setInterval(() => {
+      window.location.reload();
+    }, 1500);
   }
-  // this.context.clearRect(0, 0, 1200, 800);
-  // let img = new Image();
-  // img.src = './images/exampleSite/fireball.png';
-  // img.onload = function () {
-  //   this.context.drawImage(img, 330, 280);
-  // }.bind(this);
-
 }
 
